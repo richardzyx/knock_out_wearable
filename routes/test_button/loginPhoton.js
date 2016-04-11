b84c58a5e9f452d1a0f66a92a4537cdd755dd20d
richardz <yuxuan.zhang@tufts.edu>
2016-04-10 21:15:24 -0400

    return list of devices

--- routes/test_button/loginPhoton.js
+++ routes/test_button/loginPhoton.js
@@ -8,14 +8,24 @@ var router = express.Router();
 var Particle = require('particle-api-js');
 var particle = new Particle();
 
-router.get('/', function(req, res, next) {
+router.use('*', function(req, res, next) {
     var username = req.query.username;
     var password = req.query.password;
     particle.login({username: username, password: password}).then(
         function onFulfilled(data){
             console.log('API call completed on promise resolve: ', data.body.access_token);
+            return data.body.access_token;
         }
-    ).catch(function(err){
+    ).then(function(token){
+        var devicesPr = particle.listDevices({ auth: token });
+        devicesPr.then(function onFulfilled(devices){
+            console.log('Devices: ', devices);
+            return devices;
+        }, function(err){
+            throw err;
+        });
+
+    }).catch(function(err){
         console.log('API call completed on promise fail: ', err);
     });
 });
