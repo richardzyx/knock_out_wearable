0525e6cadff3ea6af899173f36889320ec4e42bd
richardz <yuxuan.zhang@tufts.edu>
2016-04-10 21:42:05 -0400

    called function

--- routes/test_button/loginPhoton.js
+++ routes/test_button/loginPhoton.js
@@ -11,20 +11,32 @@ var particle = new Particle();
 router.use('*', function(req, res, next) {
     var username = req.query.username;
     var password = req.query.password;
+    var token;
     particle.login({username: username, password: password}).then(
         function onFulfilled(data){
             console.log('API call completed on promise resolve: ', data.body.access_token);
-            return data.body.access_token;
+            token = data.body.access_token;
+            return token;
         }
     ).then(function(token){
         var devicesPr = particle.listDevices({ auth: token });
-        devicesPr.then(function onFulfilled(devices){
-            console.log('Devices: ', devices);
+        return devicesPr.then(function onFulfilled(devices){
+            console.log('Devices: ', devices.body[0].id);
             return devices;
         }, function(err){
             throw err;
         });
+    }).then(function(devices){
+        var device_id = devices.body[0].id;
+        var fnPr = particle.callFunction({ deviceId: device_id, name: 'click', argument: 'string', auth: token });
 
+        return fnPr.then(function(data) {
+            console.log('Function called succesfully:', data);
+            res.render('test_button', { title: 'KNOCKOUT' , message: JSON.stringify(data)});
+        }, function(err) {
+            console.log('An error occurred:', err);
+            throw err;
+        });
     }).catch(function(err){
         console.log('API call completed on promise fail: ', err);
     });
