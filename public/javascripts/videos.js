66b752ba80873b324c32907eba132e06e049c53d
annarichardson <anna.richardson@tufts.edu>
2016-04-28 22:45:02 -0400

    changed video names

--- public/javascripts/videos.js
+++ public/javascripts/videos.js
@@ -1,22 +1,20 @@
 $(document).ready(function(){
     $('button.player.player1').click(function(){
-    	if ($(this).hasClass('button_outline')) {
-    		$(this).removeClass('button_outline');
-    	} else {
-        	$(this).addClass('button_outline');
-        }
-        if($('button.player.player2').hasClass('button_outline')) {
-        	$('button.player.player2').removeClass('button_outline');
+        if ($(this).hasClass('button_outline')) {
+            $('button.player.player2').addClass('button_outline');
+            $(this).removeClass('button_outline');
+        } else {
+            $('button.player.player2').removeClass('button_outline');
+            $(this).addClass('button_outline');
         }
     });
     $('button.player.player2').click(function(){
     	if ($(this).hasClass('button_outline')) {
+            $('button.player.player1').addClass('button_outline');
     		$(this).removeClass('button_outline');
     	} else {
-        	$(this).addClass('button_outline');
-        }
-        if($('button.player.player1').hasClass('button_outline')) {
         	$('button.player.player1').removeClass('button_outline');
+            $(this).addClass('button_outline');
         }
     });
     var classNames, socket;
