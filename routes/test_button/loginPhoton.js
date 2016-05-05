8fb7189da5ddd1e3aaac9a13ad2790ab5113df08
richardz <yuxuan.zhang@tufts.edu>
2016-05-05 12:29:04 -0400

    cleanup

--- routes/test_button/loginPhoton.js
+++ routes/test_button/loginPhoton.js
@@ -32,35 +32,6 @@ router.use('*', function(req, res, next) {
     }).catch(function(err){
         console.log('API call completed on promise fail: ', err);
     });
-
-    // .then(function(devices){
-    //     device_id = devices.body[0].id;
-    //     // var fnPr = particle.callFunction({ deviceId: device_id, name: 'change_rate', argument: '10', auth: token });
-    //     res.render('test_button', { title: 'KNOCKOUT' , message: device_id});
-    //     // resolve();
-    //     // return device_id;
-    //     // return fnPr.then(function(suc) {
-    //     //     console.log("fn0 complete");
-    //     //     return device_id;
-    //     // });
-    // })
-    // .then(function(device_id){
-    //     var fnPr = particle.callFunction({ deviceId: device_id, name: 'change_rate', argument: '40', auth: token });
-    //
-    //     return fnPr.then(function(suc) {
-    //         console.log("fn0 complete");
-    //         return device_id;
-    //     });
-    // }).then(function(device_id){
-    //     var fnPr2 = particle.callFunction({ deviceId: device_id, name: 'parse_motor', argument: '02010000040001', auth: token });
-    //
-    //     return fnPr2.then(function(suc) {
-    //         console.log("fn1 complete");
-    //         res.render('test_button', { title: 'KNOCKOUT' , message: JSON.stringify(suc)});
-    //     }, function(err) {
-    //         throw err;
-    //     });
-    // })
 });
 
 
